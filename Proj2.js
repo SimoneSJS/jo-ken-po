0a6f0ea9eec8d18b39529c3949501fa858506f4b
Simone <simone.simone200862@gmail.com>
2021-08-29 22:44:38 -0300

    entrega

--- Proj2.js
+++ Proj2.js
@@ -1,8 +1,33 @@
-const prompt = require('prompt-sync')();
-let pergunta = prompt('Você escolhe pedra?')
-if (pergunta === 'pedra') {
-    console.log('Você respondeu pedra!')
-  
-} else {
-    console.log('Você respondeu !')
+let opcao = ['Pedra','Papel','Tesoura']
+
+let = jogador(prompt('Pedra',' Papel',' Tesoura'))
+let = Computador(prompt('Pedra','Papel','Tesoura'))
+prompt('jogador escolheu opcao ')
+prompt('Computador escolheu opcao ')
+
+
+if (computador == 0 && jogador == 2){
+    console.log('COMPUTADOR VENCEU!')
+}
+else if (computador == 1 && jogador == 0){
+    console.log('COMPUTADOR VENCEU!')
+}
+
+if (computador == 1 && jogador ==  0){
+    console.log('COMPUTADOR VENCEU!')
+}
+else if (computador == 2 && jogador == 1){
+    console.log('COMPUTADOR VENCEU!')
+}
+if (computador == 1 && jogador ==  0){
+    console.log('VOCÊ VENCEU!')
+}
+else if (jogador ==  0 && jogador ==  2){
+    console.log('EMPATE!')
+}
+if (jogador ==  1 && jogador == 0){
+    console.log("EMPATE!")
+}
+else if (computador == 0 && jogador == 2){
+    console.log("VOCÊ VENCEU!")
 }
